61821721fe964444eb961e0efd90783772341f0f
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2024-03-07 18:16:14 +0530

    Data pass from mfe as Cart App to Host App

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from 'react'
 function App({dataFromHostForCart}) {
+  const [cartData, setCartData] = useState('');
   console.log("in cartApp===", dataFromHostForCart?.itemData?.itemName)
+  const handleChange = (event) => {
+    const cartNawData = event.target.value;
+    setCartData(cartNawData);
+    if (typeof dataFromHostForCart?.onCartDataChange === 'function') {
+      dataFromHostForCart?.onCartDataChange(cartNawData);
+    }
+  };
   return (
     <div className="App">
       <h1>Cart App</h1>
       <p>{dataFromHostForCart?.itemData?.itemName}</p>
+      <input type="text" value={cartData} onChange={handleChange} />
     </div>
   );
 }
--- host/src/App.js
+++ host/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import DashboardApp from './mfeapp/DashboardApp';
 import CartApp from './mfeapp/CartApp';
 function App() {
+  const [cartNewData, setCartnewData]= useState('');
   let personObj = {firstName:"John", lastName:"Doe", age:20, eyeColor:"black"};
   let itemObj = {item:"Mobile", itemName:"iPhone", price:560000, color:"black"};
   const handleChildDataChange = (newData) => {
-    console.log("=====in host==", newData);
+    console.log("=====in host from Dashboard==", newData);
+  };
+  const handleCartDataChnage = (data) => {
+    setCartnewData(data)
+    console.log("=====in host from cart==", data);
   };
   return (
     <div>
       <p>Host App</p>
       <DashboardApp myDatat={"abcd"} personObj={personObj} title={'From Host'} onChildDataChange={handleChildDataChange} ></DashboardApp>
-      <CartApp itemData={itemObj}></CartApp>
+      <CartApp itemData={itemObj} onCartDataChange={handleCartDataChnage}></CartApp>
+      <p>In Side host App : {cartNewData}</p>
     </div>
   );
 }
